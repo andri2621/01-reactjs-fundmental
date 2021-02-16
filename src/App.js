b076836af2c4d948a27e3ab184d439aec3478a7f
kangdiancp <kangdiancp@gmail.com>
2021-02-16 09:21:23 +0700

    doen

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import CounterArrow from './components/CounterArrow'
 import CounterHook from './components/CounterHook'
 import MainLayout from './containers/MainLayout'
 import { BrowserRouter } from 'react-router-dom';
+import AppLayout from './containers/AppLayout'
 
 
 function App() {
   return (
     <BrowserRouter>
-        <MainLayout />
+        <AppLayout />
       </BrowserRouter>
 
   );
